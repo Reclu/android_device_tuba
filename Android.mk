3184a4e832fd66cca01fc09eff1cfa7675f456e9
Adrien Renaud <adrien.renaud@ec-nantes.fr>
2019-08-11 10:23:05 +0200

    deleting the repo

--- Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),tuba)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo full_tuba-user
-add_lunch_combo full_tuba-eng
